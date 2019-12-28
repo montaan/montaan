6bce402a2212bb0d112392f90299b7c2ea4cfe16
Ilmari Heikkinen <hei@heichen.hk>
2019-12-28 15:59:55 +0800

    src/repo.js: /_/repo/diff {repo:string, hash:string} to get a commit diff

--- src/repo.js
+++ src/repo.js
@@ -15,6 +15,16 @@ function assertRepoFile(fsPath) {
     return [null, {filePath, stat}];
 }
 
+function assertRepoDir(fsPath) {
+    const filePath = path.resolve('repos', fsPath);
+    const reposPath = path.resolve('repos');
+    if (!filePath.startsWith(reposPath+'/')) return ['403: Access Denied', {}];
+    if (!fs.existsSync(filePath)) return ['404: File Not Found', {}];
+    const stat = fs.statSync(filePath);
+    if (!stat.isDirectory()) return ['403: Access Denied', {}];
+    return [null, {filePath, stat}];
+}
+
 const Repos = {
     create: async function(req, res) {
         var [error, { user_id }] = await guardPostWithSession(req); if (error) return error;
@@ -49,6 +59,20 @@ const Repos = {
         var [error, { filePath }] = assertRepoFile(path.join(repo, 'index.csearch')); if (error) return error;
         await new Promise((resolve, reject) => {
             exec(`CSEARCHINDEX='${filePath}' $HOME/go/bin/csearch -i -n '${query}' | sed -E 's:^.+/repo/::'`, async function(error, stdout, stderr){
+                res.writeHeader(200, {'Content-Type': 'text/plain'});
+                await res.end(stdout || '');
+                resolve();
+            });
+        });
+    },
+
+    diff: async function(req, res) {
+        var [error, { repo, hash }] = assertShape({repo:isString, hash:isString}, await bodyAsJson(req)); if (error) return error;
+        if (!/^[a-f0-9]+$/.test(hash)) return '400: Malformed hash';
+        var [error, { filePath }] = assertRepoDir(path.join(repo, 'repo')); if (error) return error;
+        await new Promise((resolve, reject) => {
+            exec(`cd ${filePath} && git diff ${hash}~1 ${hash}`, async function(error, stdout, stderr){
+                if (error) reject(error);
                 res.writeHeader(200, {'Content-Type': 'text/plain'});
                 await res.end(stdout || '');
                 resolve();
