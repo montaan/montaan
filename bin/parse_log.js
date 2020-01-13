fd485dc7c4e9bf211915cad02d077e7dbff5d10c
Ilmari Heikkinen <hei@heichen.hk>
2020-01-13 23:08:46 +0800

    fix parse_log when last char of block is newline

--- bin/parse_log.js
+++ bin/parse_log.js
@@ -15,6 +15,8 @@ function parseCommits(instream, outstream) {
     var lineBufs = [];
     var lineBufSize = 0;
 
+    var lineNum = 0;
+
     function parseBlock(block) {
         for (var i = 0; i < block.length; ++i) {
             while (block[i] !== 10 && i < block.length) ++i;
@@ -26,37 +28,36 @@ function parseCommits(instream, outstream) {
                     lineBufs.splice(0);
                 }
                 const line = buf.slice(lineStart, lineBufSize + i).toString();
-                parseLine(line, buf[lineStart]);
-                lineStart = i+1;
+                if (line.length > 0) {
+                    if (commitRE.test(line)) {
+                        if (sha) {
+                            const commit = {sha, author, message: message.join("\n"), date, files};
+                            // if (!author) console.error(commit, lineNum, lineStart, lineBufSize, lastCommit);
+                            outstream.write(commitSep + JSON.stringify(commit));
+                            commitSep = nextCommitSep;
+                        }
+                        sha = line.substring(7);
+                        author = date = undefined;
+                        message = [];
+                        files = [];
+                    }
+                    else if (authorRE.test(line)) author = line.substring(8);
+                    else if (author && !date) date = new Date(line.substring(6)).getTime();
+                    else if (author && date) {
+                        if (buf[lineStart] === 32) message.push(line.replace(messagePrefixRE, ''));
+                        else {
+                            const [action, path, renamed] = line.split("\t");
+                            files.push({action, path, renamed});
+                        }
+                    }
+                }
+                lineNum++;
+                lineStart = (i+1 < block.length) ? i+1 : 0;
                 lineBufs.splice(0);
                 lineBufSize = 0;
             } else {
                 lineBufs.push(block);
                 lineBufSize += block.length;
-            }
-        }
-    }
-
-    function parseLine(line, firstChar) {
-        if (line.length === 0) return;
-        if (commitRE.test(line)) {
-            if (sha) {
-                const commit = {sha, author, message: message.join("\n"), date, files};
-                outstream.write(commitSep + JSON.stringify(commit));
-                commitSep = nextCommitSep;
-            }
-            sha = line.substring(7);
-            author = date = undefined;
-            message = [];
-            files = [];
-        }
-        else if (authorRE.test(line)) author = line.substring(8);
-        else if (author && !date) date = new Date(line.substring(6));
-        else if (author && date) {
-            if (firstChar === 32) message.push(line.replace(messagePrefixRE, ''));
-            else {
-                const [action, path, renamed] = line.split("\t");
-                files.push({action, path, renamed});
             }
         }
     }
