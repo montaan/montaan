21247144d17dc66bd576c6a0055b6f30a4ad0768
Ilmari Heikkinen <hei@heichen.hk>
2020-01-31 00:03:04 +0800

    [backend] Disable eslint

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,10 +1,16 @@
 module.exports = {
     root: true,
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
+    "env": {
+       "node": true,
+    },
+    // parser: '@typescript-eslint/parser',
+    // plugins: ['@typescript-eslint'],
     extends: [
         'eslint:recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
+        // 'plugin:@typescript-eslint/eslint-recommended',
+        // 'plugin:@typescript-eslint/recommended',
     ],
+    "parserOptions": {
+       "ecmaVersion": 2022
+    },
 };
