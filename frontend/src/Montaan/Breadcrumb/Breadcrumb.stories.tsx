6b32271b02ee7faa5c54015acc31e15584d9c2fd
Ilmari Heikkinen <hei@heichen.hk>
2020-02-08 15:35:32 +0800

    [frontend] Stories fixing

--- frontend/src/Montaan/Breadcrumb/Breadcrumb.stories.tsx
+++ frontend/src/Montaan/Breadcrumb/Breadcrumb.stories.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { storiesOf } from '@storybook/react';
 import Breadcrumb from './';
 
 storiesOf('Breadcrumb', module).add('Breadcrumb', () => (
 	<div>
-		<Breadcrumb navigationTarget="/foo/bar" fileTree={{}} />
+		<Router>
+			<Breadcrumb navigationTarget="/foo/bar" fileTree={{}} />
+		</Router>
 	</div>
 ));
--- frontend/src/Montaan/RepoSelector/RepoSelector.stories.tsx
+++ frontend/src/Montaan/RepoSelector/RepoSelector.stories.tsx
@@ -1,22 +1,43 @@
 import * as React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { storiesOf } from '@storybook/react';
+import 'bootstrap/dist/css/bootstrap.css';
+
 import RepoSelector from './';
 
-storiesOf('RepoSelector', module).add('RepoSelector', () => (
+storiesOf('Montaan/RepoSelector', module).add('RepoSelector', () => (
 	<div>
-		<h1>
-			The RepoSelector component is a dropdown to show list of repos and create new ones for
-			easy navigation between user's repos.
-		</h1>
-		<p>The RepoSelector component is used by the MainApp screen.</p>
-		<p>The primary reviewer for RepoSelector is Ilmari Heikkinen {'<hei@heichen.hk>'}.</p>
-		<RepoSelector
-			repos={[{ name: 'foo', owner: 'bar', url: '', commit_count: 20, processing: false }]}
-			createRepo={async (name: string, url?: string) =>
-				new Promise((r) =>
-					r({ name: 'foo', owner: 'bar', url: '', commit_count: 20, processing: false })
-				)
-			}
-		/>
+		<div style={{ width: '50%' }}>
+			<h3>RepoSelector</h3>
+			<p>
+				The RepoSelector component is a dropdown to show list of repos and create new ones
+				for easy navigation between user's repos.
+			</p>
+			<p>The RepoSelector component is used by the MainApp screen.</p>
+			<p>The primary reviewer for RepoSelector is Ilmari Heikkinen {'<hei@heichen.hk>'}.</p>
+
+			<h4>API</h4>
+		</div>
+
+		<hr />
+
+		<Router>
+			<RepoSelector
+				repos={[
+					{ name: 'foo', owner: 'bar', url: '', commit_count: 20, processing: false },
+				]}
+				createRepo={async (name: string, url?: string) =>
+					new Promise((r) =>
+						r({
+							name: 'foo',
+							owner: 'bar',
+							url: '',
+							commit_count: 20,
+							processing: false,
+						})
+					)
+				}
+			/>
+		</Router>
 	</div>
 ));
