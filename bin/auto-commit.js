3938115cf97048d7a969ec8da1cfafbb56b48149
Ilmari Heikkinen <hei@heichen.hk>
2020-02-22 12:15:40 +0800

    [bin] undefined
    Edited bin/auto-commit.js

--- bin/auto-commit.js
+++ bin/auto-commit.js
@@ -31,50 +31,60 @@ function getStatusMessage() {
   });
 }
 
-getStatusMessage().then(function(statusMessage) {
-  const lines = statusMessage.split("\n").filter(l => !/^\?\?/.test(l));
-  const tags = {};
-  const addTag = (tag, path) => (tags[tag] = tags[tag] || []).push(path);
-  const paths = [];
-  lines.forEach(l => {
-    if (!l) return;
-    let [_, action, path] = l.match(/^\s*(\S+)\s+(.*)/);
-    if (path.startsWith('"')) path = JSON.parse(path);
-    tagPatterns.forEach(
-      ([tag, pattern]) => pattern.test(path) && addTag(tag, path)
-    );
-    const dir = path.split("/")[0];
-    if (dir) {
-      const name =
-        dir.split(".")[0].toLowerCase() || dir.split(".")[1].toLowerCase();
-      if (name) addTag(name, path);
-    }
-    paths.push([action, path]);
-  });
-  console.log(
-    Object.keys(tags)
-      .sort()
-      .map(t => `[${t}]`),
-    paths
-  );
+setInterval(
+  () =>
+    getStatusMessage().then(function(statusMessage) {
+      const lines = statusMessage.split("\n").filter(l => !/^\?\?/.test(l));
+      if (lines.length === 0) {
+        console.log("No changes");
+        return;
+      }
+      const tags = {};
+      const addTag = (tag, path) => (tags[tag] = tags[tag] || []).push(path);
+      const paths = [];
+      lines.forEach(l => {
+        if (!l) return;
+        let [_, action, path] = l.match(/^\s*(\S+)\s+(.*)/);
+        if (path.startsWith('"')) path = JSON.parse(path);
+        tagPatterns.forEach(
+          ([tag, pattern]) => pattern.test(path) && addTag(tag, path)
+        );
+        const dir = path.split("/")[0];
+        if (dir) {
+          const name =
+            dir.split(".")[0].toLowerCase() || dir.split(".")[1].toLowerCase();
+          if (name) addTag(name, path);
+        }
+        paths.push([action, path]);
+      });
+      console.log(
+        Object.keys(tags)
+          .sort()
+          .map(t => `[${t}]`),
+        paths
+      );
 
-  const messageLine =
-    Object.keys(tags)
-      .sort()
-      .map(t => `[${t}]`)
-      .join("") +
-    " " +
-    process.argv[2];
-  const messageBody = paths
-    .map(([action, path]) => actionToString(action) + " " + path)
-    .join("\n");
-  const commitMessage = messageLine + "\n\n" + messageBody;
-  var bash = spawn("bash");
-  bash.stdin.end('git commit -m "' + commitMessage.replace(/"/g, '\\"') + '"');
-  bash.stdout.on("data", function(data) {
-    console.log(data.toString());
-  });
-  bash.stderr.on("data", function(data) {
-    console.log(data.toString());
-  });
-});
+      const messageLine =
+        Object.keys(tags)
+          .sort()
+          .map(t => `[${t}]`)
+          .join("") +
+        " " +
+        process.argv[2];
+      const messageBody = paths
+        .map(([action, path]) => actionToString(action) + " " + path)
+        .join("\n");
+      const commitMessage = messageLine + "\n\n" + messageBody;
+      var bash = spawn("bash");
+      bash.stdin.end(
+        'git commit -m "' + commitMessage.replace(/"/g, '\\"') + '"'
+      );
+      bash.stdout.on("data", function(data) {
+        console.log(data.toString());
+      });
+      bash.stderr.on("data", function(data) {
+        console.log(data.toString());
+      });
+    }),
+  60000
+);
