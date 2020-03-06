af998431f225749a74bb658ec21b5550bd043a0f
Ilmari Heikkinen <hei@heichen.hk>
2020-03-06 15:10:51 +0800

    [frontend] Port test gen utilities to TS

---
+++ frontend/bin/updateComponents.ts
@@ -0,0 +1,27 @@
+#!yarn ts-node
+
+import { updateComponent } from './updateComponent';
+import path from 'path';
+import glob from 'glob';
+
+if (!module.parent) {
+	const files = process.argv.slice(2);
+	updateComponents(files);
+}
+
+export function updateComponents(files: string[]): void {
+	const components = new Map<string, number>();
+	files.forEach((file) => {
+		const dir = path.dirname(file);
+		const stories = glob.sync(path.join(dir, '*.stories.tsx'));
+		if (stories.length > 0) {
+			components.set(dir, 1);
+		}
+	});
+	for (let dir of components.keys()) {
+		const segments = dir.split(path.sep);
+		const target = segments[segments.length - 2];
+		const name = segments[segments.length - 1];
+		updateComponent(target, name);
+	}
+}
