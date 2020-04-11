262202eadf696fb2cf0d3016392c7ade20a5e431
Ilmari Heikkinen <hei@heichen.hk>
2020-04-11 16:25:21 +0800

    [frontend] Fix Breadcrumb test

--- frontend/src/Montaan/Breadcrumb/Breadcrumb.test.tsx
+++ frontend/src/Montaan/Breadcrumb/Breadcrumb.test.tsx
@@ -4,11 +4,16 @@ import { render } from '@testing-library/react';
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Breadcrumb from './';
+import { FSDirEntry } from '../lib/filesystem';
 
 test('renders without crashing', () => {
 	const { baseElement } = render(
 		<Router>
-			<Breadcrumb navigationTarget="/foo/bar/baz" fileTree={{}} />
+			<Breadcrumb
+				navigationTarget="/foo/bar/baz"
+				fileTree={{ count: 0, tree: new FSDirEntry() }}
+				fileTreeUpdated={1}
+			/>
 		</Router>
 	);
 	expect(baseElement).toBeInTheDocument();
